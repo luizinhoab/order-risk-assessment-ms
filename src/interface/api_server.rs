01d8106251d8eb016371e496a02ceae103026800
luizinhoab <luizinhoab@gmail.com>
2020-05-12 20:15:51 -0300

    #4 removing empty mod tests

--- src/interface/api_server.rs
+++ src/interface/api_server.rs
@@ -1,7 +1,6 @@
 use super::operation_handlers as handlers;
-use std::env::var;
 use actix_web::{web, App, HttpServer};
-
+use std::env::var;
 
 pub async fn create_server() -> std::io::Result<()> {
     let host = var("SERVER_HOST").unwrap_or_else(|_| String::from("localhost"));
@@ -9,22 +8,24 @@ pub async fn create_server() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         App::new()
-            .route("/api/doc", web::get().to(|r| handlers::handle_statics_by_path("doc/swaggerui/index.html".to_string(), r)))
-            .route("/api/risk-api.yaml", web::get().to(|r| handlers::handle_statics_by_path("doc/risk-api.yaml".to_string(), r)),)
+            .route(
+                "/api/doc",
+                web::get().to(|r| {
+                    handlers::handle_statics_by_path("doc/swaggerui/index.html".to_string(), r)
+                }),
+            )
+            .route(
+                "/api/risk-api.yaml",
+                web::get()
+                    .to(|r| handlers::handle_statics_by_path("doc/risk-api.yaml".to_string(), r)),
+            )
             .route(
                 "/api/{filename:.*}",
                 web::get()
                     .to(|r| handlers::handle_statics_by_path("doc/swaggerui/".to_string(), r)),
             )
-
     })
-        .bind(&format!("{}:{}", host, port))?
-        .run()
-        .await
-
-}
-
-#[cfg(test)]
-mod tests {
-
+    .bind(&format!("{}:{}", host, port))?
+    .run()
+    .await
 }
