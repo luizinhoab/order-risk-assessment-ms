d571e67150acc80c257d1e3c2e1ab59377b763cd
luizinhoab <luizinhoab@gmail.com>
2020-05-12 05:33:29 -0300

    #4 initial setup for api server with routes and respecives operations to api docs

---
+++ src/interface/api_server.rs
@@ -0,0 +1,30 @@
+use super::operation_handlers as handlers;
+use std::env::var;
+use actix_web::{web, App, HttpServer};
+
+
+pub async fn create_server() -> std::io::Result<()> {
+    let host = var("SERVER_HOST").unwrap_or_else(|_| String::from("localhost"));
+    let port = var("SERVER_PORT").unwrap_or_else(|_| String::from("8000"));
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/api/doc", web::get().to(|r| handlers::handle_statics_by_path("doc/swaggerui/index.html".to_string(), r)))
+            .route("/api/risk-api.yaml", web::get().to(|r| handlers::handle_statics_by_path("doc/risk-api.yaml".to_string(), r)),)
+            .route(
+                "/api/{filename:.*}",
+                web::get()
+                    .to(|r| handlers::handle_statics_by_path("doc/swaggerui/".to_string(), r)),
+            )
+
+    })
+        .bind(&format!("{}:{}", host, port))?
+        .run()
+        .await
+
+}
+
+#[cfg(test)]
+mod tests {
+
+}
