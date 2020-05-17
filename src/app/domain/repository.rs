3fbc9299bf510ee8b7d64615b46283629577a169
luizinhoab <luizinhoab@gmail.com>
2020-05-17 03:31:03 -0300

    #8 supporting for mock

--- src/app/domain/repository.rs
+++ src/app/domain/repository.rs
@@ -1,3 +1,7 @@
-pub trait Repository<T, K> {
+#[cfg(test)]
+use mockall::*;
+
+#[cfg_attr(test, automock)]
+pub trait Repository<T: 'static, K: 'static> {
     fn save(&self, object: T) -> Result<T, K>;
 }
