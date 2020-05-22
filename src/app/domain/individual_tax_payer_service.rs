92287955b95d7e59e7079f58207c34777e755311
luizinhoab <luizinhoab@gmail.com>
2020-05-22 00:01:03 -0300

    #6 add service to search information by inscription number

---
+++ src/app/domain/individual_tax_payer_service.rs
@@ -0,0 +1,8 @@
+use crate::app::domain::models::IndividualTaxPayer;
+use crate::errors::CustomError;
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait IndividualTaxPayerService {
+    async fn find_payer_by_number(number: String) -> Result<IndividualTaxPayer, CustomError>;
+}
